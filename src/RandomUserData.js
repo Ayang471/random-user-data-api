311a4d511c612925fa866c7207ea6c9080434b58
github-aliceyang <aliyang47@gmail.com>
2023-06-12 12:48:05 -0700

    added the form and button

--- src/RandomUserData.js
+++ src/RandomUserData.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function RandomUserData() {
-  return (
-    <div>RandomUserData</div>
-  )
+
+   
+    return (
+        <div className='generate-random-user'>
+            <div className='container'>
+                <form onSubmit={HandleSubmit}>
+                    <button className='button'>Generate Random User Info</button>
+                    <div className='main'>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default RandomUserData
